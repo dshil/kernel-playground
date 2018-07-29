0e0c1cc4d4477343eb6aef5c8a4c0ff5498f7b57
Dmitriy Shilin <sdadeveloper@gmail.com>
2018-07-29 08:06:22 +0300

    screen: rename constants

--- drivers/screen.h
+++ drivers/screen.h
@@ -1,17 +1,17 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-#define VIDEO_ADDRESS 0xB8000
+#define SCREEN_VIDEO_ADDRESS 0xB8000
 
 #define MAX_ROWS 25
 #define MAX_COLS 80
 
 #define COLOR_SCHEME_WHITE_ON_BLACK 0x0F
 
-#define REG_SCREEN_CTRL 0x3D4
-#define REG_SCREEN_DATA 0x3D5
-#define REG_SCREEN_CURSOR_HIGH_BYTE 0x0E
-#define REG_SCREEN_CURSOR_LOW_BYTE 0x0F
+#define SCREEN_PORT_CTL 0x3D4
+#define SCREEN_PORT_DATA 0x3D5
+#define SCREEN_PORT_SET_CURSOR_HIGH_BYTE 0x0E
+#define SCREEN_PORT_SET_CURSOR_LOW_BYTE 0x0F
 
 void clear_screen(void);
 void printk(const char *fmt, ...);
